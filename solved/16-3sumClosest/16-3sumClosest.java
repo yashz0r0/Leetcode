0a0b4a041e6b6d4c194103216e6b12d461b35cef
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-06-22 23:55:05 +0530

    Create 16-3sumClosest

---
+++ solved/16-3sumClosest/16-3sumClosest.java
@@ -0,0 +1,27 @@
+// Last updated: 22/06/2026, 23:55:06
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+       Arrays.sort(nums);
+       int n=nums.length;
+       int res=nums[0]+nums[1]+nums[2];
+
+       for(int i=0;i<n-2;i++){
+        int l=i+1;
+        int r=n-1;
+
+        while(l<r){
+            int sum=nums[i]+nums[l]+nums[r];
+            if(Math.abs(target-sum)<Math.abs(target-res)){
+                res=sum;
+            }
+            if(sum==target) return sum;
+            else if(sum<target) l++;
+            else r--;
+        }
+       }
+
+
+       return res;
+        
+    }
+}
