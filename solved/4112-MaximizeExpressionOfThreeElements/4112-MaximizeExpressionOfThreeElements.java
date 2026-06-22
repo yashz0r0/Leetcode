4a2e09de194377265cf6084b153e025723df4600
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-06-23 00:00:28 +0530

    Create 4112-MaximizeExpressionOfThreeElements

---
+++ solved/4112-MaximizeExpressionOfThreeElements/4112-MaximizeExpressionOfThreeElements.java
@@ -0,0 +1,10 @@
+// Last updated: 23/06/2026, 00:00:30
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        Arrays.sort(nums);
+        int n= nums.length;
+
+        return nums[n-1]+nums[n-2]-nums[0];
+        
+    }
+}
