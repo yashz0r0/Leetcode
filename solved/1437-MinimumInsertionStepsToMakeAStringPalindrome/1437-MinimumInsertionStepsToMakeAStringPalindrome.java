e96acba132107927f5ac1ab04968d22bc9e998e0
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-06-23 00:03:25 +0530

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ solved/1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -0,0 +1,23 @@
+// Last updated: 23/06/2026, 00:03:26
+class Solution {
+    public int minInsertions(String a) {
+          StringBuilder b= new StringBuilder(a);
+        b.reverse();
+
+        int m=a.length();
+        int n=b.length();
+        int[][] t=new int[m+1][n+1];
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    t[i][j]=1+t[i-1][j-1];
+                }else{
+                    t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return n-t[m][n];
+        
+    }
+}
