863fadd0bbf18dbb2d0207e6e8d160e4953d351d
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-06-22 23:53:37 +0530

    Create 242-ValidAnagram

---
+++ solved/242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,29 @@
+// Last updated: 22/06/2026, 23:53:39
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // count chars from s
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (!map.containsKey(ch)) return false;
+
+            if (map.get(ch) == 1) {
+                map.remove(ch);
+            } else {
+                map.put(ch, map.get(ch) - 1);
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
