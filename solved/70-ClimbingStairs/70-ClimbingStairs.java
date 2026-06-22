c2a8709a1ec8b8b9b7df3020621c14b5b8b2a20b
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-06-22 23:54:45 +0530

    Create 70-ClimbingStairs

---
+++ solved/70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,18 @@
+// Last updated: 22/06/2026, 23:54:46
+class Solution {
+    public int climbStairs(int n) {
+
+        if(n <= 2) return n;
+
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
