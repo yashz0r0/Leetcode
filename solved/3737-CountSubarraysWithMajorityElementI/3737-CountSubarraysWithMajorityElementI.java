2416bb01d895a412945218aff5484b9b10aa0a15
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-06-25 22:15:44 +0530

    File updated at 25/06/2026, 22:15:46

--- solved/3737-CountSubarraysWithMajorityElementI/3737-CountSubarraysWithMajorityElementI.java
+++ solved/3737-CountSubarraysWithMajorityElementI/3737-CountSubarraysWithMajorityElementI.java
@@ -1,21 +1,18 @@
-// Last updated: 25/06/2026, 22:01:45
+// Last updated: 25/06/2026, 22:15:46
 1class Solution {
 2    public int countMajoritySubarrays(int[] nums, int target) {
-3        int n=nums.length;
-4        int ans=0;
+3        int n = nums.length;
+4        int ans = 0;
 5
-6
-7        for(int l=0;l<n;l++){
-8            int tarcount=0;
-9
-10            for(int r=l;r<n;r++){
-11                if(nums[r]==target)tarcount++;
-12
-13                int len=r-l+1;
-14                if(tarcount>len/2)ans++;
-15            }
-16        }
-17
-18        return ans;
-19    }
-20}
+6        for (int l = 0; l < n; l++) {
+7            int count = 0;
+8            for (int r = l; r < n; r++) {
+9                if (nums[r] == target) count++;
+10                int len = r - l + 1;
+11                if (count > len / 2) ans++;
+12            }
+13        }
+14
+15        return ans;
+16    }
+17}
