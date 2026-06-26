1eca62d9d65ef582ec2f70bcf2c804b173b46326
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-06-26 15:20:17 +0530

    File updated at 26/06/2026, 15:20:19

--- solved/560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ solved/560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,4 +1,4 @@
-// Last updated: 26/06/2026, 15:09:58
+// Last updated: 26/06/2026, 15:20:19
 1class Solution {
 2    public int subarraySum(int[] nums, int k) {
 3       int n=nums.length;
@@ -8,12 +8,20 @@
 7       map.put(0,1);
 8       for(int i=0;i<n;i++){
 9        sum+=nums[i];
-10
-11        if(map.containsKey(sum-k))
-12        ans+=map.get(sum-k);
-13        map.put(sum,map.getOrDefault(sum,0)+1);
-14       }
-15
-16       return ans;
-17    }
-18}
+10        //  System.out.println("step "+(i+1)+": "+sum);
+11        if(map.containsKey(sum-k)){
+12          ans+=map.get(sum-k);
+13        //   System.out.println("inside contains and sum-k "+ (sum-k));
+14        //   System.out.println(ans);
+15        //   System.out.println((sum-k)+" "+map.get(sum-k));
+16        }
+17        map.put(sum,map.getOrDefault(sum,0)+1);
+18        //  System.out.println("Map");
+19        //  for(int key:map.keySet()){
+20        //      System.out.println(key+" "+map.get(key) );
+21        //  }
+22       }
+23
+24       return ans;
+25    }
+26}
