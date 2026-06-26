3db7b06de403de4b4a3d7fa52e268c0b356b4277
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-06-26 14:47:39 +0530

    File updated at 26/06/2026, 14:47:41

--- solved/724-FindPivotIndex/724-FindPivotIndex.java
+++ solved/724-FindPivotIndex/724-FindPivotIndex.java
@@ -1,16 +1,14 @@
-// Last updated: 22/06/2026, 23:55:52
-class Solution {
-    public int pivotIndex(int[] nums) {
-        int n= nums.length;
-        int total=0;
-        int sum=0;
-
-        for(int x:nums)total+=x;
-
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(sum==(total-(sum-nums[i]))) return i;
-        }
-        return -1;
-    }
-}
+// Last updated: 26/06/2026, 14:47:41
+1class Solution {
+2    public int pivotIndex(int[] nums) {
+3       int total=Arrays.stream(nums).sum();
+4       int sum=0;
+5       for(int i=0;i<nums.length;i++){
+6         sum+=nums[i];
+7        if(sum==(total-sum+nums[i]))return i;
+8         
+9       }
+10
+11       return -1;
+12    }
+13}
