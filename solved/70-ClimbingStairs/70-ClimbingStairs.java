5dc574a73b018b426a347d6dd190052a86386275
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-07-14 12:11:51 +0530

    File updated at 14/07/2026, 12:11:48

--- solved/70-ClimbingStairs/70-ClimbingStairs.java
+++ solved/70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,18 +1,21 @@
-// Last updated: 22/06/2026, 23:54:46
-class Solution {
-    public int climbStairs(int n) {
-
-        if(n <= 2) return n;
-
-        int[] dp = new int[n+1];
-
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-
-        return dp[n];
-    }
-}
+// Last updated: 14/07/2026, 12:11:48
+1class Solution {
+2    int dp[];
+3    public int climbStairs(int n) {
+4      dp= new int[n+1];
+5       Arrays.fill(dp,-1);
+6
+7       return solve(n);
+8        
+9    }
+10    public int solve(int n){
+11         if(n==0) return 0;
+12        if(n==1) return 1;
+13        if(n==2)return 2;
+14
+15        if(dp[n]!=-1)return dp[n];
+16
+17
+18        return dp[n]=solve(n-1)+solve(n-2);
+19    }
+20}
