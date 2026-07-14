27ee6e21a962c0a0f40cf09ef714495166e723c0
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-07-14 12:07:21 +0530

    Create 509-FibonacciNumber

---
+++ solved/509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 14/07/2026, 12:07:19
+1class Solution {
+2    int dp[];
+3    public int fib(int n) {
+4       dp= new int[n+1];
+5       Arrays.fill(dp,-1);
+6
+7       return solve(n);
+8        
+9    }
+10    public int solve(int n){
+11         if(n==0) return 0;
+12        if(n==1) return 1;
+13
+14        if(dp[n]!=-1)return dp[n];
+15
+16
+17        return dp[n]=fib(n-1)+fib(n-2);
+18    }
+19}
