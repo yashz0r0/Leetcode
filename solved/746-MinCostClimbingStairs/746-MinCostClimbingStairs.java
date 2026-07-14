36aaf89a694efaab81afabbe7c9f555d74d85905
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-07-14 12:24:12 +0530

    File updated at 14/07/2026, 12:24:10

--- solved/746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
+++ solved/746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
@@ -1,24 +1,17 @@
-// Last updated: 14/07/2026, 12:23:34
+// Last updated: 14/07/2026, 12:24:10
 1class Solution {
-2    int cost[];
-3    int[] dp;
-4    public int minCostClimbingStairs(int[] cost) {
-5        this.cost=cost;
-6        int n=cost.length;
-7        dp= new int[n+1];
-8       Arrays.fill(dp,-1);
+2    public int minCostClimbingStairs(int[] cost) {
+3        int n=cost.length;
+4
+5        int dp[] = new int[n];
+6        dp[0]=cost[0];
+7        dp[1]=cost[1];
+8
 9
-10       return solve(n);
-11        
-12    }
-13    public int solve(int n){
-14         if(n==0) return cost[0];
-15        if(n==1) return cost[1];
-16
-17        if(dp[n]!=-1)return dp[n];
-18        if(n==cost.length)  return dp[n] = Math.min(solve(n - 1), solve(n - 2));
-19        
-20        return dp[n]=cost[n]+Math.min(solve(n-1),solve(n-2));
-21    }
-22
-23}
+10        for(int i=2;i<n;i++){
+11            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+12        }
+13
+14        return Math.min(dp[n-1],dp[n-2]);
+15    }
+16}
