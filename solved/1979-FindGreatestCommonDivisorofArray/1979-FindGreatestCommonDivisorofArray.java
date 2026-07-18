735a8a749f754e498576b136bdd0bd5885aaffea
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-07-18 19:16:15 +0530

    File updated at 18/07/2026, 19:16:15

--- solved/1979-FindGreatestCommonDivisorofArray/1979-FindGreatestCommonDivisorofArray.java
+++ solved/1979-FindGreatestCommonDivisorofArray/1979-FindGreatestCommonDivisorofArray.java
@@ -1,16 +1,15 @@
-// Last updated: 18/07/2026, 19:13:38
+// Last updated: 18/07/2026, 19:16:15
 1class Solution {
 2    public int findGCD(int[] nums) {
-3
-4        Arrays.sort(nums);
-5
-6        return gcd(nums[0],nums[nums.length-1]);
-7        
-8    }
-9
-10    private int gcd(int a, int b){
-11        if(b==0) return a;
-12
-13        return gcd(b, a%b);
-14    }
-15}
+3        int min=Arrays.stream(nums).min().getAsInt();
+4        int max=Arrays.stream(nums).max().getAsInt();
+5        return gcd(min,max);
+6        
+7    }
+8
+9    private int gcd(int a, int b){
+10        if(b==0) return a;
+11
+12        return gcd(b, a%b);
+13    }
+14}
