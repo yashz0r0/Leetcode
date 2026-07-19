77c3198eb16a7323265200de458f9e25aa533d08
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-07-19 18:04:11 +0530

    File updated at 19/07/2026, 18:04:11

--- solved/496-NextGreaterElementI/496-NextGreaterElementI.java
+++ solved/496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -1,22 +1,27 @@
-// Last updated: 22/06/2026, 23:53:02
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stack = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums2) {
-            while (!stack.isEmpty() && stack.peek() < num) {
-                map.put(stack.pop(), num);
-            }
-            stack.push(num);
-        }
-
-  
-        int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = map.getOrDefault(nums1[i], -1);
-        }
-
-        return result;
-    }
-}
+// Last updated: 19/07/2026, 18:04:11
+1class Solution {
+2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+3        Stack<Integer> st = new Stack<>();
+4        Map<Integer, Integer> map = new HashMap<>();
+5
+6        for (int i = nums2.length - 1; i >= 0; i--) {
+7            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+8                st.pop();
+9            }
+10
+11            if (st.isEmpty())
+12                map.put(nums2[i], -1);
+13            else
+14                map.put(nums2[i], st.peek());
+15
+16            st.push(nums2[i]);
+17        }
+18
+19        int[] res = new int[nums1.length];
+20        for (int i = 0; i < nums1.length; i++) {
+21            res[i] = map.get(nums1[i]);
+22        }
+23
+24        return res;
+25    }
+26}
