40a1675739f46a35ec7233145249908fa4fa2caf
Yash <149415979+yashz0r0@users.noreply.github.com>
2026-07-20 19:19:58 +0530

    File updated at 20/07/2026, 19:19:58

--- solved/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values.java
+++ solved/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values/3572-MaximizeY‑SumbyPickingaTripletofDistinctX‑Values.java
@@ -1,26 +1,26 @@
-// Last updated: 20/07/2026, 19:14:46
+// Last updated: 20/07/2026, 19:19:58
 1class Solution {
 2    class Pair{
-3        int val,idx;
-4        Pair(int val,int idx){
-5            this.val=val;
-6            this.idx=idx;
+3        int y,x;
+4        Pair(int y,int x){
+5            this.y=y;
+6            this.x=x;
 7        }
 8    }
 9    public int maxSumDistinctTriplet(int[] x, int[] y) {
-10        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->Integer.compare(b.val,a.val));
+10        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->Integer.compare(b.y,a.y));
 11        for(int i=0;i<y.length;i++){
-12            pq.add(new Pair(y[i],i));
+12            pq.add(new Pair(y[i],x[i]));
 13        }
 14        long sum=0;
 15        HashSet<Integer> hs=new HashSet<>();
 16        int cnt=0;
 17        while(cnt<3 && !pq.isEmpty()){
 18            Pair obj=pq.poll();
-19            if(!hs.contains(x[obj.idx])){
-20                sum+=obj.val;
+19            if(!hs.contains(obj.x)){
+20                sum+=obj.y;
 21                cnt++;
-22                hs.add((x[obj.idx]));
+22                hs.add(obj.x);
 23            }
 24        }
 25        return cnt==3?(int)sum:-1;
